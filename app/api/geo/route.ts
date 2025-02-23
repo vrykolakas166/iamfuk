ac9d3b10cec4a5b1edf92e89cebdde38c27b6cfd
vrykolakas166 <hhtb.bdh@gmail.com>
2025-02-23 16:03:31 +0700

    fix

--- app/api/geo/route.ts
+++ app/api/geo/route.ts
@@ -1,13 +1,7 @@
-import { geolocation, ipAddress } from "@vercel/functions";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export function GET(request: Request) {
-  //DEBUG START: checking incoming request
-  console.log("Headers:", request.headers);
-  console.log("IP:", ipAddress(request));
-  //DEBUG END: checking incoming request
-
-  const { city } = geolocation(request);
+export function GET(request: NextRequest) {
+  const city = request.headers.get("X-User-City") || "Hanoi";
 
   return NextResponse.json({ city });
 }
--- app/api/test/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export function GET(req: NextRequest) {
-  // Lấy toàn bộ headers của request
-  const headers = Object.fromEntries(req.headers.entries());
-
-  // Trả về JSON hiển thị tất cả headers
-  return NextResponse.json({ headers }, { status: 200 });
-}
