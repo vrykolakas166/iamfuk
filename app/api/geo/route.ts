4806c3cec522a38c5b6ec284d86daccd5fadeed9
vrykolakas166 <hhtb.bdh@gmail.com>
2025-02-23 15:54:03 +0700

    deployment debugging

--- app/api/geo/route.ts
+++ app/api/geo/route.ts
@@ -1,7 +1,12 @@
-import { geolocation } from "@vercel/functions";
+import { geolocation, ipAddress } from "@vercel/functions";
 import { NextResponse } from "next/server";
 
 export function GET(request: Request) {
+  //DEBUG START: checking incoming request
+  console.log("Headers:", request.headers);
+  console.log("IP:", ipAddress(request));
+  //DEBUG END: checking incoming request
+
   const { city } = geolocation(request);
 
   return NextResponse.json({ city });
--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,21 @@ import { type NextRequest } from "next/server";
 import { updateSession } from "@/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  const response = await updateSession(request);
+
+  // Get geolocation
+  const country = request.headers.get("x-vercel-ip-country") || "Unknown";
+  const region = request.headers.get("x-vercel-ip-country-region") || "Unknown";
+  const city = request.headers.get("x-vercel-ip-city") || "Unknown";
+
+  console.log(`User Location: ${city}, ${region}, ${country}`);
+
+  // Assign location
+  response.headers.set("X-User-City", city);
+  response.headers.set("X-User-Region", region);
+  response.headers.set("X-User-Country", country);
+
+  return response;
 }
 
 export const config = {
